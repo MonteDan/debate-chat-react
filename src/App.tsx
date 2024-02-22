8414f58861a892a6e91b748e966e4d4136a879e1
MonteBro <czech.illusion@gmail.com>
2024-02-22 23:20:12 +0100

    feat: use react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,33 @@
 import Help from "@/components/Help";
+import Create from "@/pages/Create";
 import Home from "@/pages/Home";
+import Admin from "@/pages/Admin";
+import Chat from "@/pages/Chat";
 import { MessageSquareText } from "lucide-react";
+import { Link, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <>
       <nav className="fixed top-0 left-0 bg-background w-full flex justify-between px-8 py-5">
-        <a
-          href="/"
+        <Link
+          to="/"
           className="flex gap-2 items-center font-serif text-2xl font-semibold"
         >
           <MessageSquareText className="text-primary" size={48} />
           Debatní chat
-        </a>
+        </Link>
 
         <Help></Help>
       </nav>
 
       <main className="min-h-screen w-screen flex justify-center items-center py-32">
-        <Home></Home>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/create" element={<Create />} />
+          <Route path="/admin/:chat_id" element={<Admin />} />
+          <Route path="/chat/:chat_id" element={<Chat />} />
+        </Routes>
       </main>
     </>
   );
