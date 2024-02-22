a69d109b7ebd5d7080d2842f402f744a4d8b3dbb
MonteBro <czech.illusion@gmail.com>
2024-02-22 23:20:00 +0100

    wip: homepage

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,26 +1,64 @@
-import { Button } from "@/components/ui/button";
+import { Button, buttonVariants } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+} from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
-import { LogIn } from "lucide-react";}
+import { Separator } from "@/components/ui/separator";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { LogIn, Plus } from "lucide-react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import {Link} from 'react-router-dom';
+
+const joinSchema = z.object({
+  id: z.string().min(1),
+});
 
 function Home() {
+  const joinForm = useForm<z.infer<typeof joinSchema>>({
+    resolver: zodResolver(joinSchema),
+  });
+
+  const onSubmit = (values: z.infer<typeof joinSchema>) => {
+    console.log(values);
+  };
+
   return (
     <div className="flex flex-col gap-16 w-full max-w-lg">
       <div className="flex flex-col gap-1.5">
-        <Label for="id">Zadejte adresu a připojte se jako divák</Label>
-        <form className="flex gap-2">
-          <Input
-            type="text"
-            name="id"
-            className="w-full"
-            placeholder="Adresa chatu"
-            // value={$form.id}
-          />
+        <Form {...joinForm}>
+          <form
+            onSubmit={joinForm.handleSubmit(onSubmit)}
+            className="flex items-end gap-2"
+          >
+            <FormField
+              control={joinForm.control}
+              name="id"
+              render={({ field }) => (
+                <FormItem className='space-y-0 w-full'>
+                  <FormLabel htmlFor="id">
+                    Zadejte adresu a připojte se jako divák
+                  </FormLabel>
+                  <FormControl>
+                    <Input
+                      className="w-full"
+                      placeholder="Adresa chatu"
+                      {...field}
+                    />
+                  </FormControl>
+                </FormItem>
+              )}
+            />
 
-          <Button type="submit">
-            <LogIn className="mr-3"></LogIn> Vstoupit
-          </Button>
-        </form>
+            <Button type="submit">
+              <LogIn className="mr-3" /> Vstoupit
+            </Button>
+          </form>
+        </Form>
         {/* {#if $errors.id}
 			<p className="text-destructive">{$errors.id}</p>
 		{/if} */}
@@ -28,9 +66,9 @@ function Home() {
 
       <Separator className="w-full"></Separator>
 
-      <Button variant="ghost" href="/create">
+      <Link to="/create" className={buttonVariants({ variant: "ghost" })}>
         <Plus className="mr-3"></Plus> Vytvořit Chat
-      </Button>
+      </Link>
     </div>
   );
 }
